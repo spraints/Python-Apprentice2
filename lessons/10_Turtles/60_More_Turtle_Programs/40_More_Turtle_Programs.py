1221de543ad9cc40ee64ea447fe4751c51d52049
Matt Burke <spraints@gmail.com>
2025-11-10 19:09:29 +0000

    save progress

--- lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
@@ -13,3 +13,35 @@ Use this code to get a random x and y location
     y = random.randint(-300, 300)
 
 """
+
+import turtle as turtle
+
+turtle.setup(width=600, height=600)
+
+t = turtle.Turtle()
+
+t.shape("turtle")
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4) # Make the turtle really big
+
+def turtle_clicked(t, x, y):
+    """Function that gets called when the user clicks on the turtle
+
+    This function will make the turtle tilt 20 degrees 18 times, making a full
+    circle. It is called by the turtle when the user clicks on it.
+
+    Args:
+        t (Turtle): The turtle object that was clicked
+        x (int): The x coordinate of the click
+        y (int): The y coordinate of the click
+    """
+
+    print('turtle clicked!')
+    
+    for i in range(0,360, 20): # Full circle, 20 degrees at a time
+        t.tilt(20) # Tilt the turtle 20 degrees
+
+# Connect the turtle to the turtle_clicked function
+t.onclick(lambda x, y, t=t: turtle_clicked(t, x, y))
+
+
+turtle.done() # Important! Use `done` not `exitonclick` to keep the window open
